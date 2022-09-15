d45a672527702427ef0f8be116cbb2f3172e6eda
Mridul Kumar <kumar.mridul000@gmail.com>
2022-09-15 18:51:13 +0530

    seup customize-cra with babel for private-methods

--- front-end/config-overrides.js
+++ front-end/config-overrides.js
@@ -1,9 +1,17 @@
-module.exports = function override(webpackConfig) {
-  webpackConfig.module.rules.push({
-    test: /\.mjs$/,
-    include: /node_modules/,
-    type: "javascript/auto"
-  });
+// Copyright 2019-2020 @Premiurly/polkassembly authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
 
-  return webpackConfig;
-}
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-undef */
+/* eslint-disable no-tabs */
+const { override,	addWebpackModuleRule, useBabelRc } = require('customize-cra');
+
+module.exports = override(
+	useBabelRc(),
+	addWebpackModuleRule({
+		include: '/node_modules/',
+		test: /\.mjs$/,
+		type: 'javascript/auto'
+	})
+);
